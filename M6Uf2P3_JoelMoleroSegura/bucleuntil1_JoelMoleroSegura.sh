53fd91149ec478da21f4ea00aa657c4e60a7430c
Joel Molero Segura <jmoleros@ies-sabadell.cat>
2024-02-06 08:57:22 +0100

    Update bucleuntil1_JoelMoleroSegura.sh

--- M6Uf2P3_JoelMoleroSegura/bucleuntil1_JoelMoleroSegura.sh
+++ M6Uf2P3_JoelMoleroSegura/bucleuntil1_JoelMoleroSegura.sh
@@ -3,12 +3,14 @@
 
 # Comprova si el nombre de paràmetres és igual a 3
 until [ "$#" -eq 3 ]
-# Aquesta és l'inici del bucle 'until'. Comprova si el nombre de paràmetres ($#) és igual a 3.
 do
     echo "Error: El nombre de paràmetres ha de ser 3. Has introduït $# paràmetres."
     # Si el nombre de paràmetres no és 3, aquesta línia mostra un missatge d'error.
+    echo "Por favor, introduce tres parámetros:"
+    read -a params
+    set -- "${params[@]}"
 done
-# Aquesta és la fi del bucle 'until'.
 
 echo "Has introduït 3 paràmetres correctament."
 # Si el nombre de paràmetres és 3, aquesta línia mostra un missatge de confirmació.
+
